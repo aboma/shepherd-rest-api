6bad1373b4eb2e745def24af05ff8ff51721e266
Adrian Boma <avboma@gmail.com>
2013-03-20 16:07:50 -0400

    embed asset in relationship serializer

--- app/presenters/v1/relationship_serializer.rb
+++ app/presenters/v1/relationship_serializer.rb
@@ -2,10 +2,10 @@ module V1
   class RelationshipSerializer < ActiveModel::Serializer
     #include Rails.application.routes.url_helpers
     
-    attributes :id, :relationship_type, :portfolio_id, :asset_id, :created_at, :updated_at
+    attributes :id, :relationship_type, :portfolio_id, :created_at, :updated_at #, :asset_id
     
     # include the asset definition in the relationship
-    #has_one :asset, :key => :asset, :serializer => V1::AssetSerializer
+    has_one :asset, :key => :asset, :serializer => V1::AssetSerializer, :embed => :objects
     
   end
 end
